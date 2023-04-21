84742fef2aee100656569d9ecf7250cdb24e7e5f
hasankarim18 <hallysust@gmail.com>
2023-04-21 22:40:22 +0600

    showing categories

--- src/Pages/Shared/LeftNavigation/LeftNavigation.jsx
+++ src/Pages/Shared/LeftNavigation/LeftNavigation.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNavigation = () => {
   const [categories, setCategories] = useState([])
@@ -6,16 +7,25 @@ const LeftNavigation = () => {
 
   useEffect(() => {
     fetch("http://localhost:5000/categories")
-    .then(res=> res.json())
-    .then(data => setCategories(data))
-    .catch(errro => console.log(error))
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((errro) => console.log(errro));
   }, [])
 
   console.log(categories);
 
     return (
       <div>
-        <h2 className="text-danger">Left navigation</h2>
+        <h4 className="text-dark">All Categories</h4>
+        <div className="ps-4">
+          {
+            categories.map(category => (
+              <p key={category.id}>
+                <Link className="text-decoration-none nav-link" to={`/category/${category.id}`}>{category.name} </Link>
+              </p>
+            ))
+          }
+        </div>
       </div>
     );
 };
