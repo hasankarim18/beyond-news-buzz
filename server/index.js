f9c780ef7f17f2ece981a5a916bc92119c9b6081
hasankarim18 <hallysust@gmail.com>
2023-04-21 12:53:02 +0600

    solving cors by installing npm install cors and require it to the server index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const categories = require('./data/categories.json')
+var cors = require("cors");
+
 
 const app = express()
 const port = process.env.port ||  5000; 
+app.use(cors());
 
 app.get('/', (req, res)=> {
     res.send('Beyond News Buzz running')
 })
+
+app.get('/categories', (req, res)=> {
+    res.send(categories);
+} )
 
 
 app.listen(port, ()=> {
--- src/Pages/Shared/LeftNavigation/LeftNavigation.jsx
+++ src/Pages/Shared/LeftNavigation/LeftNavigation.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNavigation = () => {
+  const [categories, setCategories] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+    .then(res=> res.json())
+    .then(data => setCategories(data))
+    .catch(errro => console.log(error))
+  }, [])
+
+  console.log(categories);
+
     return (
       <div>
         <h2 className="text-danger">Left navigation</h2>
